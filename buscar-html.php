853c0ce76d1d9dd57c1253ef56572a56c7bf4426
Gabriel <gabrielvieira840@gmail.com>
2020-05-14 16:53:50 -0300

    COMMIT

---
+++ buscar-html.php
@@ -0,0 +1,17 @@
+<?php
+    require 'vendor/autoload.php';
+
+    use GuzzleHttp\Client;
+    use Symfony\Component\DomCrawler\Crawler;
+    use GabrielVieira\BuscadorDeHTML\Buscador;
+    
+    $client = new Client(['base_uri' => 'https://www.alura.com.br']);
+    $crawler = new Crawler();
+
+    $buscador = new Buscador($client, $crawler);
+    $corpo = $buscador->buscador('/cursos-online-programacao/php');
+
+    foreach($corpo as $value)
+    {
+        echo exibeMensagem($value);
+    }
---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php 
+    function exibeMensagem(string $mensagem)
+    {
+        echo "Curso: ".$mensagem . '<br>';
+    }
